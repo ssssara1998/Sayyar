04772f25b1cab0b0085050933a13e15f0259be1f
SarahAlgwaiz <60787364+SarahAlgwaiz@users.noreply.github.com>
2021-01-24 21:55:33 +0300

    test 1

--- Sayyar2021/Assets/Scripts/NewBehaviourScript.cs
+++ Sayyar2021/Assets/Scripts/NewBehaviourScript.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    private Rigidbody player;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
